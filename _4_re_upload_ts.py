ba0652289528d2a30c450d5d102c952c3d833d60
Lei Chen <41903858+3rd-stone@users.noreply.github.com>
2020-07-11 17:14:00 +0800

    Update _4_re_upload_ts.py

--- _4_re_upload_ts.py
+++ _4_re_upload_ts.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 23 14:59:45 2020
-
-@author: chenlaws
-"""
 
 import _3_compare_ts
 
 if __name__ == '__main__':
     maxThreads = 800
-    bucket = 'test--20200310'
-    prefix = 'video1-hsanhl-com/'   #最前面不要/,最后要/，比如 abc/123/
+    bucket = ''
+    prefix = '/'   #最前面不要/,最后要/，比如 abc/123/
     
     _3_compare_ts.upload_again(bucket, prefix, maxThreads)
 
