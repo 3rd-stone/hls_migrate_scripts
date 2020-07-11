33361ee41223f2c9f9438bb5c037ed35e189398d
Lei Chen <41903858+3rd-stone@users.noreply.github.com>
2020-07-11 17:12:41 +0800

    Update _2_check_error.py

--- _2_check_error.py
+++ _2_check_error.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 23 14:59:45 2020
-
-@author: chenlaws
-"""
 
 import _1_upload
 
 if __name__ == '__main__':
     maxThreads = 800
     bucket = 'test--20200310'
-    prefix = 'video1-hsanhl-com/'   #最前面不要/,最后要/，比如 abc/123/
+    prefix = '/'   #最前面不要/,最后要/，比如 abc/123/
     
     _1_upload.check_error(bucket, prefix, maxThreads)
